0d1098b771219d039b7c3fcb5f959e13831f45ac
murtazamalik <murtazamalik1319@gmail.com>
2023-03-02 21:35:22 +0530

    Adding Fibonacci Series

--- src/logicprogram/LogicProgram.java
+++ src/logicprogram/LogicProgram.java
@@ -3,6 +3,16 @@ package logicprogram;
 public class LogicProgram {
 
     public static void main(String[] args) {
-        System.out.println("Welcome To Master");
+
+        int num1 = 0;
+        int num2 = 1;
+        System.out.println(num1 + "\n" + num2);
+        for (int i = 0; i < 10; i++){
+            int num3 = num1+num2;
+            System.out.println(num3);
+            num1 = num2;
+            num2 = num3;
+        }
+
     }
 }
